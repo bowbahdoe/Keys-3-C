1efb2ddb6646c56c899f6f09701532c989549dd3
Ethan <emccue@live.com>
2016-06-23 18:27:35 -0400

    main is still just for lazy testing

--- main.c
+++ main.c
@@ -1,23 +1,61 @@
 #include "board.h"
+#include "gamestate.h"
 #include <stdio.h>
-
-int main(){
-  Board* board = default_board();
-
-  for(int x = 0;x<8 ;x++ ){
-    for(int y = 0;y<8;  y++){
-      if(is_unlocked_piece_at_location(board, x, y)){
-        printf("There is a key at (%d, %d) of team %d\n", x, y, get_unlocked_piece_at_location(board, x, y)->team);
+void print_board(Board* board){
+  for(int y = 7;y>-1 ;y-- ){
+    for(int x = 0;x<8;  x++){
+      if(is_unlocked_gold_piece_at_location(board, x, y)){
+        printf("G ");
+      }else if(is_unlocked_silver_piece_at_location(board, x, y)){
+      printf("S ");
       }else{
-        printf("There is no key at (%d, %d)\n", x, y);
+        printf("* ");
       }
     }
+    printf("  ");
+    for(int x = 0;x<8;  x++){
+      if(is_locked_gold_piece_at_location(board, x, y)){
+        printf("G ");
+      }else if(is_locked_silver_piece_at_location(board, x, y)){
+      printf("S ");
+      }else{
+        printf("* ");
+      }
+    }
+    printf("\n");
+  }
+}
+int main(){
+
+  GameState* g = new_GameState();
+
+
+
+  int x;
+  int y;
+  print_board(g->gameboard);
+  while(1){
+    printf("x: ");
+    scanf("%d", &x);
+    printf("y: ");
+    scanf("%d", &y);
+    if(x==-1){break;}
+    handle_click(g, x, y);
+    print_board(g->gameboard);
+    printf("\n%d\n",g->current_state);
   }
 
-  free_board(board);
 
   Board* n_board = new_board();
   free_board(n_board);
-
   printf("board is freed\n");
+  printf("\n");
+
+  printf("The size of a game state is %lu\n", sizeof(GameState));
+  printf("The size of a board pointer is %lu\n", sizeof(GameState*));
+  printf("The size of a board is %lu\n", sizeof(Board));
+  printf("The size of a board pointer is %lu\n", sizeof(Board*));
+  printf("The size of a key is %lu\n", sizeof(Key));
+  printf("The size of a key pointer is %lu\n", sizeof(Key*));
+
 }
